dc6e323ec0b5214ae8a98d406a52e7be104b8a13
321999 <kishorejha321999@gmail.com>
2023-12-17 21:02:09 +0530

    sorting the array logic is wirttain

--- mygoapp/handlers/handler.go
+++ mygoapp/handlers/handler.go
@@ -24,8 +24,20 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+	"sort"
+	"strings"
 )
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello, Go Server!")
+}
+func SortHandler(w http.ResponseWriter, r *http.Request) {
+	// Sample array to sort
+	arr := []string{"banana", "apple", "orange", "grape"}
+
+	// Sort the array
+	sort.Strings(arr)
+
+	// Display the sorted array
+	fmt.Fprint(w, "Sorted Array: "+strings.Join(arr, ", "))
 }
